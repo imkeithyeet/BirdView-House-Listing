3e7273c5112889ee0ab2d6f89f24bdbfa36a3cba
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-01-25 11:43:16 -0800

    styling

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 import { createGlobalStyle } from "styled-components";
 
+
 const GlobalStyle = createGlobalStyle`
   *,
   *::before, 
