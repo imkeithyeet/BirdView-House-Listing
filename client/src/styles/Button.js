ced4d3e8aab223b97238f514dc18f80b859d6179
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-01-25 08:47:11 -0800

    styling

--- client/src/styles/Button.js
+++ client/src/styles/Button.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 const COLORS = {
   primary: {
-    "--main": "indigo",
+    "--main": "SlateGray",
     "--accent": "white",
   },
   secondary: {
-    "--main": "lavenderblush",
-    "--accent": "indigo",
+    "--main": "LightGray",
+    "--accent": "black",
   },
 };
 
