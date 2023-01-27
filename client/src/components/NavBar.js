3ede4159926bccfc44712963fd07e3e95df496a3
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-01-27 09:57:47 -0800

    logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -30,7 +30,7 @@ function NavBar({ user, setUser }) {
       <Wrapper>
         <Logo>
           <Link to="/" className="logo">
-            BirdView
+          <img src="../images/logo.png" alt="logo" width={116} height={116} className="rounded-corners"/>
           </Link>
         </Logo>
         <Nav>
@@ -65,8 +65,10 @@ const Logo = styled.h1`
   text-shadow: 1px 1px 1px;
   display: inline-block;
   width: 100%;
-  // text-align: left;
-
+  text-align: left;
+   top: 70px;  
+   left: 15%;
+   border-radius: 50%;
   a {
     color: inherit;
     text-decoration: none;
@@ -80,5 +82,6 @@ const Nav = styled.nav`
   position: absolute;
   right: 8px;
 `;
+
 
 export default NavBar;
